ac72e0d3b08641ac40d62b19cab32c57ac9b76fd
Gluk <gluk@yandex-team.ru>
2013-12-09 17:31:30 +0400

    bug 944331 - bug fix update for yandex-planB, a=mconnor
    --HG--
    extra : rebase_source : 422865f5d3a8ad050b702acd6f2463a0e6803c0a

--- partners/yandex-planB/distribution/extensions/vb@yandex.ru/config.js
+++ partners/yandex-planB/distribution/extensions/vb@yandex.ru/config.js
@@ -8,8 +8,8 @@ TYPE: "vbff",
 COOKIE: "vb.ff",
 PROTOCOL: "yafd"},
 BUILD: {
-DATE: "Thu Nov 28 2013 09:43:43 GMT+0000",
-REVISION: "48423_48"},
+DATE: "Mon Dec 09 2013 13:16:16 GMT+0000",
+REVISION: "48431_29"},
 CORE: {
 CONTRACT_ID: "@yandex.ru/vb-core;1",
 CLASS_ID: Components.ID("{1ad918b4-4729-11e1-ab8a-dff4577f00a5}")}};
--- partners/yandex-planB/distribution/extensions/yasearch@yandex.ru/config.js
+++ partners/yandex-planB/distribution/extensions/yasearch@yandex.ru/config.js
@@ -8,8 +8,8 @@ NAME: APP_NAME,
 TYPE: "barff",
 COOKIE: "bar.ff"},
 BUILD: {
-DATE: "Thu Nov 28 2013 09:45:44 GMT+0000",
-REVISION: 57787},
+DATE: "Mon Dec 09 2013 08:59:27 GMT+0000",
+REVISION: 57792},
 PLATFORM: {
 VERSION: 16,
 BLACKLIST_URL: "http://export.yandex.ru/bar/blacklist.xml"},
