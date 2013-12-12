a904788efced4efbd50af6b3832e9f4bd93009c2
Gluk <gluk@yandex-team.ru>
2013-12-12 12:52:21 +0400

    bug 944331 - additional bugfixes for Yandex-planB Fx26, a=mconnor

---
+++ partners/yandex-planB/distribution/extensions/vb@yandex.ru/cbapp/parts/backup.js
@@ -0,0 +1,327 @@
+"use strict";
+const EXPORTED_SYMBOLS = ["backup"];
+const GLOBAL = this;
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+const DB_FILENAME = "fastdial.sqlite";
+const PREF_LASTDUMP_NAME = "backup.lastTime";
+const INTERVAL_SEC = 86400;
+const FILES_MAX_NUMBER = 10;
+const SYNC_THROTTLE_TIMEOUT_MS = 3000;
+const SYNC_THROTTLE_PROCESS_FINISH = "ftabs-backup-sync-finish";
+const backup = {
+init: function Backup_init(application) {
+application.core.Lib.sysutils.copyProperties(application.core.Lib,GLOBAL);
+this._application = application;
+this._logger = application.getLogger("Backup");
+this.__proto__ = new patterns.NotificationSource();
+this._initDatabase();
+var now = Math.round(Date.now() / 1000);
+var lastBackupTimeSec = this._application.preferences.get(PREF_LASTDUMP_NAME,0);
+if (! lastBackupTimeSec)
+{
+this._application.preferences.set(PREF_LASTDUMP_NAME,now);
+lastBackupTimeSec = now;
+}
+
+var diffSec = Math.abs(now - lastBackupTimeSec);
+var timeoutMs = Math.max(INTERVAL_SEC - diffSec,0) * 1000;
+new sysutils.Timer(this._dumpCurrentData.bind(this), timeoutMs, INTERVAL_SEC * 1000);
+}
+,
+finalize: function Backup_finalize(doCleanup, callback) {
+if (this._throttleTimer)
+{
+this.addListener(SYNC_THROTTLE_PROCESS_FINISH,this);
+this._finalizeDoCleanup = doCleanup;
+this._finalizeCallback = callback;
+this._throttleTimer.notify();
+return true;
+}
+
+var dbClosedCallback = (function Backup_finalize_dbClosedCallback() {
+this._finalizeDoCleanup = null;
+this._finalizeCallback = null;
+this._database = null;
+this._application = null;
+this._logger = null;
+callback();
+}
+).bind(this);
+if (this._database)
+{
+this._database.close(dbClosedCallback);
+return true;
+}
+
+dbClosedCallback();
+}
+,
+get list() {
+var dirEntries = this._backupsDir.directoryEntries;
+var files = [];
+while (dirEntries.hasMoreElements()) {
+let file = dirEntries.getNext().QueryInterface(Ci.nsIFile);
+files.push(file);
+}
+
+return files.sort(function (a, b) b.lastModifiedTime - a.lastModifiedTime).map(function (file) {
+return {
+date: file.lastModifiedTime,
+name: file.leafName};
+}
+);
+}
+,
+restore: function Backup_restore(fileName) {
+var backupFile = this._backupsDir;
+backupFile.append(fileName);
+if (! backupFile.exists() || ! backupFile.isFile() || ! backupFile.isReadable())
+throw new Error("No such backup file: " + fileName);
+var json = fileutils.jsonFromFile(backupFile);
+var thumbs = {
+};
+var newThumbs = {
+};
+this._dbTables.forEach(function (tableName) {
+this._database.execQuery("DELETE FROM " + tableName,{
+});
+}
+,this);
+this._dbTables.forEach(function (tableName) {
+json[tableName].forEach(function (row) {
+this._database.execQuery("INSERT INTO " + tableName + " (" + Object.keys(row).join(", ") + ") VALUES (" + Object.keys(row).map(function (field) ":" + field).join(", ") + ")",row);
+if (tableName === "thumbs")
+{
+thumbs[row.rowid] = row;
+}
+ else
+if (tableName === "thumbs_shown")
+{
+let dbStructure = {
+url: thumbs[row.thumb_id].url,
+title: thumbs[row.thumb_id].title,
+backgroundColor: thumbs[row.thumb_id].backgroundColor,
+favicon: thumbs[row.thumb_id].favicon,
+insertTimestamp: thumbs[row.thumb_id].insertTimestamp,
+rowid: row.thumb_id,
+syncId: row.syncId,
+syncInstance: row.syncInstance,
+syncTimestamp: row.syncTimestamp};
+newThumbs[row.position] = this._application.internalStructure.convertDbRow(dbStructure,row.fixed);
+}
+
+}
+,this);
+}
+,this);
+this._application.thumbs.resetPickupTimer();
+this._application.internalStructure.clear();
+this._application.internalStructure.setItem(newThumbs);
+this._application.fastdial.sendRequest("thumbChanged",this._application.frontendHelper.fullStructure);
+this._application.internalStructure.iterate({
+nonempty: true},function (thumbData) {
+this._application.thumbs.getMissingData(thumbData);
+}
+,this);
+}
+,
+syncThumbs: function Backup_syncThumbs() {
+if (this._throttleTimer)
+this._throttleTimer.cancel();
+this._throttleTimer = new sysutils.Timer(this._dumpThumbs.bind(this), SYNC_THROTTLE_TIMEOUT_MS);
+}
+,
+observe: function Backup_observe(aSubject, aTopic, aData) {
+switch (aTopic) {
+case SYNC_THROTTLE_PROCESS_FINISH:
+this.removeAllListeners();
+this.finalize(this._finalizeDoCleanup,this._finalizeCallback);
+break;
+}
+
+}
+,
+_initDatabase: function Backup__initDatabase() {
+var dbFile = this._application.core.rootDir;
+dbFile.append(DB_FILENAME);
+this._database = new Database(dbFile);
+}
+,
+_dumpThumbs: function Backup__dumpThumbs() {
+var self = this;
+var thumbsTasks = {
+};
+var replaceShownTasks = [];
+var thumbsRowIds;
+this._application.internalStructure.iterate(null,function (thumbData, index) {
+if (thumbData.source)
+{
+thumbsTasks[thumbData.source] = function (callback) {
+self._database.execQueryAsync("SELECT rowid, * FROM thumbs WHERE url = :url",{
+url: thumbData.source},function (rowsData, storageError) {
+if (storageError)
+return callback(storageError);
+if (rowsData.length)
+{
+let rowId = rowsData[0].rowid;
+let needsUpdate = ["title", "backgroundColor", "favicon"].some(function (field) {
+return thumbData.thumb[field] !== rowsData[0][field];
+}
+);
+if (! needsUpdate)
+return callback(null,rowId);
+self._database.execQueryAsync("UPDATE thumbs SET title = :title, backgroundColor = :backgroundColor, favicon = :favicon WHERE url = :url",{
+url: thumbData.source,
+title: thumbData.thumb.title || null,
+backgroundColor: thumbData.thumb.backgroundColor || null,
+favicon: thumbData.thumb.favicon || null},function (rowsData, storageError) {
+callback(storageError,rowId);
+}
+);
+}
+ else
+{
+self._database.execQueryAsync("INSERT INTO thumbs (url, title, backgroundImage, backgroundColor, favicon, insertTimestamp) VALUES (:url, :title, '', :backgroundColor, :favicon, :ts)",{
+url: thumbData.source,
+title: thumbData.thumb.title || null,
+backgroundColor: thumbData.thumb.backgroundColor || null,
+favicon: thumbData.thumb.favicon || null,
+ts: Math.round(Date.now() / 1000)},function (rowsData, storageError) {
+if (storageError)
+return callback(storageError);
+self._database.execQueryAsync("SELECT rowid FROM thumbs WHERE url = :url",{
+url: thumbData.source},function (rowsData, storageError) {
+if (! storageError)
+self._logger.debug("Thumb (URL: " + thumbData.source + ") was inserted into DB with rowid: " + rowsData[0].rowid);
+callback(storageError,rowsData[0].rowid);
+}
+);
+}
+);
+}
+
+}
+);
+}
+;
+}
+
+replaceShownTasks.push(function (callback) {
+self._database.execQueryAsync("INSERT OR REPLACE INTO thumbs_shown (thumb_id, position, fixed, syncId, syncInstance, syncTimestamp) VALUES(:id, :index, :pinned, :syncId, :syncInstance, :syncTimestamp)",{
+id: thumbData.source ? thumbsRowIds[thumbData.source] : 0,
+index: index,
+pinned: Number(thumbData.pinned),
+syncId: thumbData.sync.id || null,
+syncInstance: thumbData.sync.instance || null,
+syncTimestamp: thumbData.sync.timestamp || null},function (rowsData, storageError) {
+callback(storageError);
+}
+);
+}
+);
+}
+,this);
+async.parallel(thumbsTasks,function (storageError, thumbs) {
+if (storageError)
+{
+let errorMsg = strutils.formatString("Get thumbs rowid error: %1 (code %2)",[storageError.message, storageError.result]);
+self._logger.error(errorMsg);
+return;
+}
+
+thumbsRowIds = thumbs;
+self._database.execQueryAsync("SELECT MAX(rowid) AS rowid FROM thumbs_shown",{
+},function (rowsData, storageError) {
+if (storageError)
+throw new Error(strutils.formatString("Get max thums_shown rowid error: %1 (code %2)",[storageError.message, storageError.result]));
+if (rowsData.length)
+{
+replaceShownTasks.push(function (callback) {
+self._database.execQueryAsync("DELETE FROM thumbs_shown WHERE rowid <= :maxint",{
+maxint: rowsData[0].rowid},function (rowsData, storageError) {
+callback(storageError);
+}
+);
+}
+);
+}
+
+async.parallel(replaceShownTasks,function (storageError) {
+if (storageError)
+{
+let errorMsg = strutils.formatString("Replace shown error: %1 (code %2)",[storageError.message, storageError.result]);
+self._logger.error(errorMsg);
+return;
+}
+
+self._logger.trace("Thumbs structure synced");
+self._throttleTimer = null;
+self._notifyListeners(SYNC_THROTTLE_PROCESS_FINISH);
+}
+);
+}
+);
+}
+);
+}
+,
+_dumpCurrentData: function Backup__dumpCurrentData() {
+var self = this;
+var tasks = {
+};
+this._dbTables.forEach(function (tableName) {
+tasks[tableName] = function (callback) {
+self._database.execQueryAsync("SELECT rowid, * FROM " + tableName + " ORDER BY rowid",{
+},function (rowsData, storageError) {
+callback(storageError,rowsData);
+}
+);
+}
+;
+}
+);
+async.parallel(tasks,function Backup__initDatabase_onParallelReady(storageError, results) {
+if (storageError)
+throw new Error(strutils.formatString("Dump error: %1 (code %2)",[storageError.message, storageError.result]));
+var file = self._backupsDir;
+file.append(Date.now() + ".json");
+fileutils.jsonToFile(results,file);
+var now = Math.round(Date.now() / 1000);
+self._application.preferences.set(PREF_LASTDUMP_NAME,now);
+self._cleanOldFiles();
+}
+);
+}
+,
+_cleanOldFiles: function Backup__cleanOldFiles() {
+var dirEntries = this._backupsDir.directoryEntries;
+var dropFilesBeforeTime = Date.now() - FILES_MAX_NUMBER * INTERVAL_SEC * 1000;
+while (dirEntries.hasMoreElements()) {
+let file = dirEntries.getNext().QueryInterface(Ci.nsIFile);
+if (file.lastModifiedTime < dropFilesBeforeTime)
+{
+fileutils.removeFileSafe(file);
+}
+
+}
+
+}
+,
+get _backupsDir() {
+var dir = this._application.core.rootDir;
+dir.append("backups");
+if (! dir.exists())
+{
+dir.create(Ci.nsIFile.DIRECTORY_TYPE,fileutils.PERMS_DIRECTORY);
+}
+
+return dir;
+}
+,
+_application: null,
+_logger: null,
+_database: null,
+_dbTables: ["blacklist", "thumbs", "thumbs_shown", "cloud_data", "unsafe_domains"],
+_throttleTimer: null,
+_finalizeDoCleanup: null,
+_finalizeCallback: null};
---
+++ partners/yandex-planB/distribution/extensions/vb@yandex.ru/cbapp/parts/internalStructure.js
@@ -0,0 +1,157 @@
+"use strict";
+const EXPORTED_SYMBOLS = ["internalStructure"];
+const GLOBAL = this;
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/PlacesUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+var thumbs = Object.create(null);
+const internalStructure = {
+init: function InternalStructure_init(application) {
+application.core.Lib.sysutils.copyProperties(application.core.Lib,GLOBAL);
+this._application = application;
+this._logger = application.getLogger("InternalStructure");
+Services.obs.addObserver(this,this._application.core.eventTopics.CLOUD_DATA_RECEIVED_EVENT,false);
+}
+,
+finalize: function InternalStructure_finalize(doCleanup, callback) {
+Services.obs.removeObserver(this,this._application.core.eventTopics.CLOUD_DATA_RECEIVED_EVENT);
+this._application = null;
+this._logger = null;
+}
+,
+observe: function InternalStructure_observe(aSubject, aTopic, aData) {
+switch (aTopic) {
+case this._application.core.eventTopics.CLOUD_DATA_RECEIVED_EVENT:
+aData = JSON.parse(aData);
+this.iterate({
+nonempty: true},function (thumbData, index) {
+try {
+if (thumbData.location.asciiHost !== aData.domain)
+return;
+delete aData.domain;
+this.setItem(index,{
+cloud: aData});
+let requestData = {
+};
+requestData[index] = this._application.frontendHelper.getDataForIndex(index);
+this._application.fastdial.sendRequest("thumbChanged",requestData);
+}
+catch (ex) {
+
+}
+
+}
+,this);
+break;
+}
+
+}
+,
+setItem: function InternalStructure_setItem(index, value) {
+if (arguments.length === 1)
+{
+for(let [index, value] in Iterator(arguments[0])) {
+this.overwriteItem(index,value);
+}
+
+}
+ else
+{
+if (! value)
+return;
+thumbs[index] = thumbs[index] || {
+};
+sysutils.copyProperties(value,thumbs[index]);
+this._application.backup.syncThumbs();
+}
+
+}
+,
+overwriteItem: function InternalStructure_overwriteItem(index, value) {
+if (arguments.length === 1)
+{
+for(let [index, value] in Iterator(arguments[0])) {
+this.overwriteItem(index,value);
+}
+
+}
+ else
+{
+thumbs[index] = value;
+this._application.backup.syncThumbs();
+}
+
+}
+,
+getItem: function InternalStructure_getItem(index) {
+return thumbs[index];
+}
+,
+removeItem: function InternalStructure_removeItem(index) {
+delete thumbs[index];
+this._application.backup.syncThumbs();
+}
+,
+clear: function InternalStructure_clear() {
+thumbs = Object.create(null);
+this._application.backup.syncThumbs();
+}
+,
+iterate: function InternalStructure_iterate(options, callback, ctx) {
+options = options || {
+};
+var currentThumbsNum = this._application.layout.getThumbsNum();
+for(let [index, thumbData] in Iterator(thumbs)) {
+if (options.visible && index >= currentThumbsNum)
+continue;
+if (options.nonempty && (! thumbData || ! thumbData.source))
+continue;
+if (options.pinned && (! thumbData || ! thumbData.pinned))
+continue;
+callback.call(ctx,thumbData,index);
+}
+
+}
+,
+convertDbRow: function InternalStructure_convertDbRow(thumbData, pinned) {
+var output = {
+pinned: Boolean(pinned)};
+if (thumbData)
+{
+let locationObj = this._application.fastdial.getDecodedLocation(thumbData.url);
+sysutils.copyProperties(locationObj,output);
+output.thumb = {
+};
+output.sync = {
+};
+["title", "backgroundColor", "favicon", "visits"].forEach(function (fieldName) {
+if (thumbData[fieldName])
+{
+output.thumb[fieldName] = thumbData[fieldName];
+}
+
+}
+);
+let host = locationObj.location ? locationObj.location.asciiHost : null;
+output.cloud = host ? this._application.cloudSource.getCachedExistingData(host) : {
+};
+["Id", "Instance", "Timestamp"].forEach(function (fieldName) {
+var dbField = "sync" + fieldName;
+if (thumbData[dbField])
+{
+output.sync[fieldName.toLowerCase()] = thumbData[dbField];
+}
+
+}
+);
+}
+
+return output;
+}
+,
+get length() {
+return Object.keys(thumbs).length;
+}
+,
+_application: null,
+_logger: null};
