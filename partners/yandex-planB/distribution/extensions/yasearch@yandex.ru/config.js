4c2c53420e5533c5edd33af46849ecbd58c56ca3
Gluk <gluk@yandex-team.ru>
2014-04-21 11:44:09 +0400

    bug 994587 - yandex-planB updates for fx29, a=mconnor
    --HG--
    extra : rebase_source : c6f9e7dfc75ae0279adaca5eed703f040e2e3161

--- partners/yandex-planB/distribution/extensions/yasearch@yandex.ru/config.js
+++ partners/yandex-planB/distribution/extensions/yasearch@yandex.ru/config.js
@@ -9,8 +9,8 @@ const CB_CONFIG = function () {
                     COOKIE: "bar.ff"
                 },
                 BUILD: {
-                    DATE: "Wed Apr 09 2014 10:08:25 GMT+0000",
-                    REVISION: 54201
+                    DATE: "Thu Apr 17 2014 11:54:42 GMT+0000",
+                    REVISION: 54202
                 },
                 PLATFORM: { VERSION: 19 },
                 CORE: {
