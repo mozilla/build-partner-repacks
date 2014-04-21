ff51b6b08ade6cb809fdd8a558bf547444b1eb67
Mike Kaply <mozilla@kaply.com>
2014-04-21 15:36:14 -0400

    bug 992975: Partner repack changes for web.de, gmx, and mail.com (Firefox 29). r=mconnor

--- partners/mail.com/distribution/extensions/toolbar@mail.com/defaults/preferences/unitedinternet-general.js
+++ partners/mail.com/distribution/extensions/toolbar@mail.com/defaults/preferences/unitedinternet-general.js
@@ -1,5 +1,5 @@
 // Prevent firefox from fetching our metadata from AMO web service, but let it use our install.rdf
 pref("extensions.toolbar@mail.com.getAddons.cache.enabled", false);
-pref("ext.noCoexistenceWarning", false);
-pref("ext.firstrun", true);
-//pref("ext.currentversion", "none");
+pref("extensions.unitedinternet.ext.noCoexistenceWarning", false);
+pref("extensions.unitedinternet.ext.firstrun", true);
+//pref("extensions.unitedinternet.ext.currentversion", "none");
