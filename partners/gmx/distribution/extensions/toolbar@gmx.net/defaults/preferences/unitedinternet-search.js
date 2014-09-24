9cf9b7a2a61a7fb6571a49faf7875d7e3a799389
Mike Kaply <mozilla@kaply.com>
2014-09-24 10:13:00 -0500

    Bug #1067358 - Partner repack changes for web.de, gmx, mail.com and 1&1 (Firefox 33), r=mixedpuppy

--- partners/gmx/distribution/extensions/toolbar@gmx.net/defaults/preferences/unitedinternet-search.js
+++ partners/gmx/distribution/extensions/toolbar@gmx.net/defaults/preferences/unitedinternet-search.js
@@ -3,4 +3,4 @@ pref("extensions.unitedinternet.search.termsJSON", "[]");
 // a double-click on a word in a webpage copies that word to the search field
 // search-webselectionlistener.js
 pref("extensions.unitedinternet.search.webpageDoubleclick", true);
-pref("extensions.unitedinternet.search.goDirectlyToURLs", false);
+pref("extensions.unitedinternet.search.goDirectlyToURLs", true);
